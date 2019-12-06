cae92518775b075f042444dbe98ddaf38c6f7e50
Andika Andriana <andriana.mia1@gmail.com>
2019-12-06 09:10:31 +0700

    Tugas 8 Javascript
    Tugas ini Saya kumpulkan untuk Niomic.

---
+++ tugas8.js
@@ -0,0 +1,14 @@
+function tugas8() {
+  var andikaAndriana = {
+    jenisKelamin: "Laki-Laki",
+    programStudi: "Teknik Informatika",
+    tahunKelulusan: [2015, 2016, 2017, 2018]
+  }
+  console.log("---Data Andika Andriana---")
+  console.log(andikaAndriana);
+  console.log("---Update Data Andika Andriana---")
+  andikaAndriana.tahunKelulusan = 2018;
+  console.log(andikaAndriana);
+}
+
+tugas8();
